bf58e13b1180aff5d9d17d4a902e7217416c7757
Jennifer <jenmiu2@gmail.com>
2020-06-05 00:32:59 +0200

    check notes

--- Sign Language Digits/main.py
+++ Sign Language Digits/main.py
@@ -9,17 +9,13 @@ Lin = 1
 Lout = 4096
 num_etiquetas = 10
 x, y = processData.readFile()
-
+y = processData.correctingLabel(y)
 
 xTrain, xTest, yTrain, yTest = processData.createTrain_TestData(x, y)
 xTrainFlatten = processData.flatten(xTrain)
 xTestFlatten = processData.flatten(xTest)
 
-theta = processData.randomWeight(Lin, Lout)
-
-cost = logisticRegression.cost_function(theta, xTrainFlatten, yTrain)
-grad = logisticRegression.grad_function(theta, xTrainFlatten, yTrain)
-print("Cost: {}\r\nReg: {}\r\n".format(cost, grad))
+#theta = processData.randomWeight(Lin, Lout)
 
 
 reg = np.zeros(shape=(num_etiquetas, x.shape[1]))
@@ -27,7 +23,8 @@ for i in range(0, num_etiquetas):
     theta0 = np.zeros(shape=(Lout, 1))
     reg[i, :] = logisticRegression.oneVsAll(xTrainFlatten, yTrain, i, theta0)
 
-
+cost_history = logisticRegression.cost_history
+print("Cost History: {}".format(cost_history))
 predTest, predTrain = logisticRegression.logisticRegressionWithSklearn(xTrainFlatten, yTrain, xTestFlatten, yTest)
 print("Test Pred: {}\r\nTrain Pred: {}\r\n".format(predTest, predTrain))
 
